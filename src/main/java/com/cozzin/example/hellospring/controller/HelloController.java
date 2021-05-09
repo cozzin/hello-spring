7eb9ebd54aea5aadbdaede1b6a8a8f0d30f1c044
seongho.hong <hsh3592@gmail.com>
2021-05-09 19:34:38 +0900

    Modify hello api value

--- src/main/java/com/cozzin/example/hellospring/controller/HelloController.java
+++ src/main/java/com/cozzin/example/hellospring/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "hello";
+        model.addAttribute("data", "spring!!"); // key value로 넣어줌
+        return "hello"; // 렌더링 할 파일명. resources/templates/hello.html. viewResolver가 화면을 찾아서 처리한다.
     }
 }
