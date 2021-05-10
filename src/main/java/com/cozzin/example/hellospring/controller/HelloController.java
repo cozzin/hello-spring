72c6901cc8d988f616629b380aa3c19df7cd37c2
seongho.hong <hsh3592@gmail.com>
2021-05-10 09:36:01 +0900

    hello api

--- src/main/java/com/cozzin/example/hellospring/controller/HelloController.java
+++ src/main/java/com/cozzin/example/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.cozzin.example.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,37 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "spring!!"); // key value로 넣어줌
         return "hello"; // 렌더링 할 파일명. resources/templates/hello.html. viewResolver가 화면을 찾아서 처리한다.
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // 템플릿 렌더링 하지 않고, 데이터를 그대로 내려줌
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // 이렇게 쓸일은 거의 없음
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
